bfb43518dff28098ba2cab48fb86921976d24949
Finn Gladstone <Finngladstone9@gmail.com>
2023-03-03 20:23:28 +1100

    Added files + scaffold

---
+++ gitm.c
@@ -0,0 +1,27 @@
+// name: Finn Gladstone
+// unikey: fgla0414
+// SID: 510 448 570
+
+#include <stdio.h>
+
+void who();
+
+void term();
+
+void resign();
+
+void view();
+
+void place(int c, int r);
+
+void history(int c, int r);
+
+// history --> hashmap? location : char array
+
+// process line of input + recognise commands 
+// incorrect command?
+
+int main()
+{
+
+} 
