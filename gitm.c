d7c9acd1b9363c119526ccb30f32bb97c279a6b6
Finn Gladstone <Finngladstone9@gmail.com>
2023-03-07 23:54:53 +1100

    Struggling with regex

--- gitm.c
+++ gitm.c
@@ -2,207 +2,119 @@
 // unikey: fgla0414
 // SID: 510 448 570
 
-/*
-    Will interpret EOF as term + exit with code 1
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <regex.h>
 
-#define BUFFER 100
-#define ARG0_BUFFER 8 // max command length = 7, + null byte
-#define NUMBER_OF_NOPARAMS 5
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 128
 #define BOARD_SIZE 19
-#define HISTORY_SIZE 100
+#define MAX_HISTORY 1200
+#define NO_PARAM_ARGS 5
 
-char board[BOARD_SIZE][BOARD_SIZE];
-int x_mist;
-int y_mist;
+/* Helper commands */
 
-int x_stone;
-int y_stone;
+void update_history(char * input) {}
 
-char player;
-char historystr[HISTORY_SIZE];
+int check_win_condition(char * board) {
+//     int reval;
+//     printf("%c wins!\n", "finn");
 
-int check_coord(int a)
-{
-    if (a >= 1 && a <= 19)
-        return 1;
-
-    printf("Invalid coordinate\n");
-    return 0;
+//     return reval;
 }
 
-int check_same_tile() 
-{
+void update_mist(int x, int y) {}
 
+
+/* Player commands */
+
+void who(char ch) {
+    printf("%c\n", ch);
 }
 
-void check_win_condition()
-{
-
+void history(char * h) {
+    printf("%s\n", h);
 }
 
-void switch_player() 
-{
-    if (player == 'B')
-        player = 'W';
-    else
-        player = 'B';
-}
+void view() {}
 
-void who() 
-{
-    printf("%c\n", player);
-}
+void place(char * input) {}
 
-void history() 
-{
-    printf("%s\n", historystr);
-}
-
-void term() 
-{
-    exit(1);
-}
-
-void resign() 
-{   
-    if (player == 'B')
-        printf("White wins!\n");
-    else   
-        printf("Black wins!\n");
-
-    history();
-    exit(0);
-}
-
-void view() 
-{
-    ;
-}
-
-void place(char * input) 
-{
-    int x;
-    int y;
-
-    if (!isalpha(input[6]) || !isupper(input[6]))
-    {
-        printf("Invalid coordinate\n");
-        return;
-    }
-
-    if (input[8] == ' ' || input[9] == ' ') // command = [place][coord][whitespace]
-    {
-        printf("Invalid!\n");
-        return;
-    }
-
-
-    x = input[6] - 'A';
-
-    if (!check_coord(x + 1)) // we read in A as 0, therefore offset for check!
-        return;
-
-    if (strlen(input) == 8) 
-        y = input[7] - '0'; 
-    else 
-        y = ((input[7] - '0') * 10) + (input[8] - '0');
-
-    if (!check_coord(y))
-        return;
-
-    if (board[x][y-1] == '.')
-        ;
-    else 
-    {
-        ;
-    }
-        
-
-    
-
-    // add to history string
-        
-    // switch_player();
-    
-} // need to clean input
-
-
-
-// https://stackoverflow.com/questions/252748/how-can-i-use-an-array-of-function-pointers
-void (*no_param_ptr[NUMBER_OF_NOPARAMS])() = {who, term, resign, view, history};
-
-void stdout_the_board() {}
-
-void update_mist() {}
+/* Main */
 
 int main()
 {
+    //Setup variables
+    char history[MAX_HISTORY];
+    char player = 'B';
+    char board[BOARD_SIZE][BOARD_SIZE];
 
-    /* Setup board + function calls*/
-
-    player = 'B';
-
-    for (int i = 0; i < 19; i++) 
-    {
-        for (int j = 0; j < 19; j++) 
-        {
+    //Setup the game board
+    
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
             board[i][j] = '.';
         }
     }
 
-    char no_param_calls[NUMBER_OF_NOPARAMS][ARG0_BUFFER];
+    // Setup regex
+    regex_t re;
+    int value; 
+    //https://regex101.com/r/rO0yE6/1
+    value = regcomp(&re, "^place [A-S]([1-9]|1[0-9]|20)$", 0);
 
-    strcpy(no_param_calls[0], "who\0");
-    strcpy(no_param_calls[1], "term\0");
-    strcpy(no_param_calls[2], "resign\0");
-    strcpy(no_param_calls[3], "view\0");
-    strcpy(no_param_calls[4], "history\0");
+    if (value != 0)
+        printf("Fucked\n");
 
-    /* Program logic loop */
+
+
 
     int c;
-    int success;
     char buffer[BUFFER_SIZE];
 
-    while (fgets(buffer, BUFFER_SIZE, stdin)) 
-    {
-        buffer[strcspn(buffer, "\n")] = 0;
-        success = 0;
+    while(fgets(buffer, BUFFER_SIZE, stdin)) {
+        
+        //remove newline
+        buffer[strcspn(buffer, "\n")] = 0; 
+        
+        if (strcmp(buffer, "who") == 0) {
+            who(player);
 
-
-        for (int i = 0; i < NUMBER_OF_NOPARAMS; i++)
-        {
-            if (strcmp(buffer, no_param_calls[i]) == 0) 
-            {
-                (*no_param_ptr[i])();
-                success = 1;
-                break;
-            }      
+        } else if (strcmp(buffer, "term") == 0) {
+            exit(1);  
         } 
-
-        if (success == 0)
-        {   
-            if (strncmp(buffer, "place ", 6) == 0)
-            {
-                if (strlen(buffer) == 9 || strlen(buffer) == 8)
-                    place(buffer);
-                else 
-                    printf("Invalid coordinate\n");
-            }
+        
+        else if (strcmp(buffer, "resign") == 0) {
+            if (player == 'B')
+                printf("White wins!\n");
             else
-                printf("Invalid!\n");
+                printf("Black wins!\n");
+
+            break;
+        } 
+        
+        else if (strcmp(buffer, "view") == 0) {
+            view();
+        } 
+        
+        else if (strncmp(buffer, "place ", 6) == 0) {
+            
+            int x = regexec(&re, "place A14", 0, NULL, 0);
+
+            printf("%d\n", x);
+
+            // if (check_win_condition)
+            //     break;
+        } 
+        
+        else {
+            printf("Invalid!\n");
         }
-
-    }
     
-    // term();
+    }
 
+    puts(history);
+    puts("Thank you for playing!");
     return 0;
-} 
+}
